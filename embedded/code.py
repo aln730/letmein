ff564b637414861eb996d8944b1c7dce9d56bc3d
AGawas <arnavgawas707@gmail.com>
2026-04-15 15:14:48 -0400

    chom

--- embedded/code.py
+++ embedded/code.py
@@ -1,51 +1,58 @@
-import gc, os, board, tinys2, digitalio, wifi, socketpool, ssl
+import board, busio, digitalio, wifi, socketpool, ssl, asyncio
 import adafruit_minimqtt.adafruit_minimqtt as MQTT
+import adafruit_character_lcd.character_lcd as characterlcd
+import adafruit_pcf8574
 from secrets import *
-from art import art_logo, art_mem_info
 from jingles import Jingle
 from app import App
 
 def main():
-    # Class for controlling the speaker and playing music
-    jingle = Jingle(board.IO4)
+    # Acckkkkk
+    ack = digitalio.DigitalInOut(board.GP15)
+    ack.direction = digitalio.Direction.INPUT
+    ack.pull = digitalio.Pull.DOWN
 
-    # Runs setup, then passes it off to loop()
-    # Show signs of life
-    art_logo()
-    art_mem_info()
-    jingle.boot_sync()
-    
-    # Set location of this device
+    # LCD
+    i2c = busio.I2C(scl=board.GP17, sda=board.GP16)
+    pcf = adafruit_pcf8574.PCF8574(i2c, address=0x27)
+    backlight = pcf.get_pin(3)
+    backlight.switch_to_output(False)
+    lcd = characterlcd.Character_LCD(
+        pcf.get_pin(0), pcf.get_pin(2), pcf.get_pin(4),
+        pcf.get_pin(5), pcf.get_pin(6), pcf.get_pin(7),
+        20, 4
+    )
+    lcd.clear()
+
+    # Jingle Jangle
+    jingle = Jingle(board.GP14)
+
     location = secrets["location"]
-    if location == '':
-        print('Location not set! Please set location.')
-        exit(1)
+    if not location:
+        print("Location not set! Please set location in secrets.")
+        return
 
-    # WiFi
-    print(f'Connecting to {secrets["ssid"]}')
+    print(f"Connecting to {secrets['ssid']}")
     print('mac address:', "%02x:%02x:%02x:%02x:%02x:%02x" % tuple(map(int, wifi.radio.mac_address)))
     wifi.radio.connect(secrets['ssid'], secrets['password'])
-    print(f'Connected to {secrets["ssid"]}!')
-    pool = socketpool.SocketPool(wifi.radio) # Create a socket pool
+    print(f"Connected! IP: {wifi.radio.ipv4_address}")
+    pool = socketpool.SocketPool(wifi.radio)
 
-    # Set up MQTT Client
+    # MQTT
     mqtt_client = MQTT.MQTT(
         broker=secrets["broker"],
         port=secrets["port"],
         socket_pool=pool,
         ssl_context=ssl.create_default_context(),
     )
-
-    print("Attempting to connect to %s" % mqtt_client.broker)
+    print(f"Connecting to MQTT broker: {mqtt_client.broker}")
     mqtt_client.connect()
-    mqtt_client.subscribe(mqtt_req_topic)
+    mqtt_client.subscribe("letmein2/req")
+    mqtt_client.subscribe("letmein2/name")
     mqtt_client.subscribe(mqtt_ack_topic)
-    mqtt_client.subscribe(mqtt_nvm_topic)
-    mqtt_client.subscribe(mqtt_timeout_topic)
 
-    app = App(mqtt_client, jingle)
+    app = App(mqtt_client, jingle, lcd, backlight, ack)
     app.launch()
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
-
